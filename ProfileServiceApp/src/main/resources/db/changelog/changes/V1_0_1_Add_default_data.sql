64efbfc1a3fd4609a192d7bdf12ebcd9ef90af28
bakob123 <bencebako123@gmail.com>
2023-06-18 16:50:27 +0200

    feat(app): add default dummy data to database

---
+++ ProfileServiceApp/src/main/resources/db/changelog/changes/V1_0_1_Add_default_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (id, name, email)
+VALUES
+  ('be4a0df0-b941-41e2-be01-0bea7a57306e', 'Default Student', 'default@email.com'),
+  ('1a43740f-8d68-4236-b000-e348b1ce2919', 'John Doe', 'johndoe@email.com'),
+  ('dace647d-f910-415b-9817-03a92fcd70f8', 'Dummy Joe', 'dummyjoe@email.com');
